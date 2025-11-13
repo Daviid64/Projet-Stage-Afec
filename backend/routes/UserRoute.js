66bebcee7fcdc84f901415a69ab2d3e37514b821
Daviid64 <david.oguihandy@gmail.com>
2025-11-13 17:07:58 +0100

    metiersAjouteCoteFront

--- backend/routes/UserRoute.js
+++ backend/routes/UserRoute.js
@@ -3,26 +3,10 @@ import userController from '../controller/userController.js';
 
 const router = express.Router();
 
-// Inscription
-router.post('/register', userController.register);
-
-// Vérification email
-router.get('/verify/:token', userController.verify);
-
-// Recherche email
-router.get('/email', userController.findByEmail);
-
 // Tous les utilisateurs
 router.get('/', userController.getAll);
 
-// Récupérer un utilisateur par ID ( Doit être après '/verify')
-router.get('/:id', userController.getById);
-
 // Suppression
 router.delete('/:id', userController.deleteById);
-router.delete('/', userController.deleteAll);
-
-// Mise à jour
-router.put('/:id', userController.updateById);
 
 export default router;
