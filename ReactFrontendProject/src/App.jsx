ac68bb99afa1d1f9199a4d25dff2a35395982091
Daviid64 <david.oguihandy@gmail.com>
2025-11-24 11:42:34 +0100

    maj3

--- ReactFrontendProject/src/App.jsx
+++ ReactFrontendProject/src/App.jsx
@@ -106,7 +106,7 @@ function App() {
           }
         />
 
-        <Route path="/forgotPassword" element={<ForgotPasswordPage />} />
+        <Route path="/forgot-Password" element={<ForgotPasswordPage />} />
         <Route path="/reset-password" element={<ResetPasswordPage />} />
 
         {/* Route protégée admin */}
--- ReactFrontendProject/src/api.js
+++ ReactFrontendProject/src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 console.log("BaseURL =", import.meta.env.VITE_API_URL)
 const API = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000',
+    baseURL: import.meta.env.VITE_API_URL || FRONTEND_URL,
 
 });
 
