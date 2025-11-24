309a18cfad768e9fd4d43b99266eeb1a48172d63
Daviid64 <david.oguihandy@gmail.com>
2025-11-24 15:46:55 +0100

    suppressionDesLogs

--- ReactFrontendProject/src/components/Login.jsx
+++ ReactFrontendProject/src/components/Login.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./Login.css";
 import API from "../api.js";
-import { Link } from "react-router-dom";
 
 export default function LoginPage() {
   const navigate = useNavigate();
