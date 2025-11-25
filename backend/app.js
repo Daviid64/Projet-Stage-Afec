ffae6af2b26aeb6b8fe9bf3a23ee4b218f506e7e
Daviid64 <david.oguihandy@gmail.com>
2025-11-25 16:25:36 +0100

    modifCSS

--- backend/app.js
+++ backend/app.js
@@ -16,12 +16,12 @@ app.use(helmet());
 app.use(
   cors({
     origin: process.env.FRONTEND_URL_PROD || process.env.FRONTEND_URL,
+    // origin: process.env.FRONTEND_URL,
     credentials: true,
     methods: ["GET", "POST", "PUT","PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
-
 
 app.use(express.json());
 
