bf187d942ec7bc5dec95c917edff4b09e8f02786
Daviid64 <david.oguihandy@gmail.com>
2025-12-26 15:13:56 +0100

    correctif

--- ReactFrontendProject/src/pages/Home.jsx
+++ ReactFrontendProject/src/pages/Home.jsx
@@ -11,16 +11,32 @@ function Acceuil() {
 
   // Charger l'utilisateur depuis localStorage
   useEffect(() => {
+   const validateUser = async () => {
     const storedUser = localStorage.getItem("user");
-    if (storedUser) {
-      try {
-        setUser(JSON.parse(storedUser));
-      } catch {
-        console.error("Erreur parsing user");
-        localStorage.removeItem("user");
-        localStorage.removeItem("token");
-      }
+    const token = localStorage.getItem("token");
+  
+    if (storedUser && token) {
+      return;
     }
+
+    try {
+      const response = await API.get("/auth/me", {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+
+      setUser(response.data.user);
+
+      localStorage.setItem("user", JSON.stringify(response.data.user));
+  } catch (err) {
+      console.error("token invalide ou expiré", err);
+
+      localStorage.removeItem("token");
+      localStorage.removeItem("user");
+      setUser(null);
+    }
+   };
+
+   validateUser();
   }, []);
 
   // Déconnexion
@@ -41,34 +57,36 @@ function Acceuil() {
   }
 };
 
-
   return (
     <div className="page-container">
       <header className="header-blue">
         <img src={logoAfec} alt="Logo AFEC" className="header-logo" />
         <nav className="header-nav">
           <Link to="/exploration" className="nav-link">Exploration des Métiers</Link>
-          {user && <button onClick={handleLogout} className="btn-logout">Déconnexion</button>}
+
+          {user && (
+            <button onClick={handleLogout} className="btn-logout">
+              Déconnexion
+              </button>
+              )}
         </nav>
       </header>
 
-      <main className="main-content">        <h2>Découvrez les métiers du numérique avec l’AFEC</h2>
-
+      <main className="main-content">       
         <h2>Découvrez les métiers du numérique avec l’AFEC</h2>
-        <p>Le numérique est partout : dans nos entreprises, nos vies et nos métiers. Mais savez-vous vraiment quelles opportunités il offre ?
-Avec l’AFEC, explorez un univers de carrières passionnantes et accessibles à tous : développement web, cybersécurité, data, et design web !
-
-À travers nos programmes de découverte, nos témoignages de professionnels et nos formations certifiantes, nous vous aidons à mieux comprendre les métiers du digital et à trouver celui qui vous correspond.
-
-
-Découvrez, apprenez et construisez votre parcours avec l’AFEC.</p>
+        <p>
+          Le numérique est partout : dans nos entreprises, nos vies et nos métiers. Mais savez-vous vraiment quelles opportunités il offre ?
+          Avec l’AFEC, explorez un univers de carrières passionnantes et accessibles à tous : développement web, cybersécurité, data, et design web !
+          À travers nos programmes de découverte, nos témoignages de professionnels et nos formations certifiantes, nous vous aidons à mieux comprendre les métiers du digital et à trouver celui qui vous correspond.
+          Découvrez, apprenez et construisez votre parcours avec l’AFEC.
+        </p>
         <div className="button-container">
           <Link to="/exploration" className="btn btn-blue">Exploration Métiers</Link>
         </div>
       </main>
 
       <footer className="footer">
-        <p>© 2025 AFEC - Tous droits réservés |{" "}</p>
+        <p>© 2025 AFEC - Tous droits réservés | </p>
         <p>
           <Link to="/mentions-legales">Mentions légales</Link> |{" "}
           <Link to="/privacy-policy">Politique de confidentialité</Link> |{" "}
